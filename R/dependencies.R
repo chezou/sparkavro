d8f0ae11c63dd3fe742a833c9ac3eaa95406def3
Michiaki Ariga <chezou+github@gmail.com>
2017-03-17 07:31:08 +0900

    Fix typo. close #1

--- R/dependencies.R
+++ R/dependencies.R
@@ -1,16 +1,16 @@
 spark_dependencies <- function(spark_version, scala_version, ...) {
-    spark_avro_vesion = ""
+    spark_avro_version = ""
     if (spark_version < "2.0.0") {
-      spark_avro_vesion = "2.0.1"
+      spark_avro_version = "2.0.1"
     } else {
-      spark_avro_vesion = "3.2.0"
+      spark_avro_version = "3.2.0"
     }
 
     sparklyr::spark_dependency(
     jars = c(
     ),
     packages = c(
-      sprintf("com.databricks:spark-avro_%s:%s", scala_version, spark_avro_vesion)
+      sprintf("com.databricks:spark-avro_%s:%s", scala_version, spark_avro_version)
     )
   )
 }
