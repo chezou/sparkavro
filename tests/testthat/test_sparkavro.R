acb4c0ec805d7ba6fcd38133c61f161fb698f3b3
Michiaki Ariga <chezou+github@gmail.com>
2017-03-16 16:26:22 +0900

    Supress warning

--- tests/testthat/test_sparkavro.R
+++ tests/testthat/test_sparkavro.R
@@ -10,14 +10,14 @@ if(!exists("sc")){
 test_that("read existing avro", {
   skip_on_cran()
 
-  df <- spark_read_avro(sc, "twitter", system.file("data", package="sparkavro"), "twitter.avro", memory = FALSE)
+  df <- spark_read_avro(sc, "twitter", system.file("inst", "extdata", package="sparkavro"), "twitter.avro", memory = FALSE)
   expect_equal(df %>% collect() %>% length(), 3)
 })
 
 test_that("write Spark DataFrame into avro", {
   skip_on_cran()
 
-  df <- spark_read_avro(sc, "twitter", system.file("data", package="sparkavro"), "twitter.avro", memory = FALSE)
+  df <- spark_read_avro(sc, "twitter", system.file("inst", "extdata", package="sparkavro"), "twitter.avro", memory = FALSE)
   df2 <- df %>% filter(username == "miguno")
   filename <- tempfile("test", fileext=".avro")
   spark_write_avro(df2, filename)
