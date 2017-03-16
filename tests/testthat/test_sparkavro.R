41a0738a9af906519375f33ed5492ebb8f0bcf92
Michiaki Ariga <chezou+github@gmail.com>
2017-03-16 13:34:03 +0900

    Add skip_on_cran()

--- tests/testthat/test_sparkavro.R
+++ tests/testthat/test_sparkavro.R
@@ -7,11 +7,15 @@ if(!exists("sc")){
 }
 
 test_that("read existing avro", {
+  skip_on_cran()
+
   df <- spark_read_avro(sc, "twitter", "./inst/extdata/twitter.avro")
   expect_equal(df %>% collect() %>% length(), 3)
 })
 
 test_that("write Spark DataFrame into avro", {
+  skip_on_cran()
+
   df2 <- df %>% filter(username == "miguno")
   filename <- tempfile("test", fileext=".avro")
   spark_write_avro(df2, filename)
