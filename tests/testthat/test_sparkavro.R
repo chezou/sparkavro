3d04ba5859379d90ef82ba081936f171480da6cb
Aki Ariga <chezou+github@gmail.com>
2017-11-17 21:58:00 +0900

    Fix test for CRAN

--- tests/testthat/test_sparkavro.R
+++ tests/testthat/test_sparkavro.R
@@ -8,15 +8,11 @@ if(!exists("sc")){
 }
 
 test_that("read existing avro", {
-  skip_on_cran()
-
   df <- spark_read_avro(sc, "twitter", system.file("extdata", package="sparkavro"), "twitter.avro", memory = FALSE)
   expect_equal(df %>% collect() %>% length(), 3)
 })
 
 test_that("write Spark DataFrame into avro", {
-  skip_on_cran()
-
   df <- spark_read_avro(sc, "twitter", system.file("extdata", package="sparkavro"), "twitter.avro", memory = FALSE)
   df2 <- df %>% filter(username == "miguno")
   filename <- tempfile("test", fileext=".avro")
