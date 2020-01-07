fb541533e8231dc498de66f8a4c46c522a649872
Johannes Tang Kristensen <johs@johs.me>
2020-01-07 13:39:49 +0100

    Fixed twitter.avro file location in test cases.

--- tests/testthat/test_sparkavro.R
+++ tests/testthat/test_sparkavro.R
@@ -8,12 +8,12 @@ if(!exists("sc")){
 }
 
 test_that("read existing avro", {
-  df <- spark_read_avro(sc, "twitter", system.file("extdata", package="sparkavro"), "twitter.avro", memory = FALSE)
+  df <- spark_read_avro(sc, "twitter", system.file("extdata", "twitter.avro", package="sparkavro"), memory = FALSE)
   expect_equal(df %>% collect() %>% length(), 3)
 })
 
 test_that("write Spark DataFrame into avro", {
-  df <- spark_read_avro(sc, "twitter", system.file("extdata", package="sparkavro"), "twitter.avro", memory = FALSE)
+  df <- spark_read_avro(sc, "twitter", system.file("extdata", "twitter.avro", package="sparkavro"), memory = FALSE)
   df2 <- df %>% filter(username == "miguno")
   filename <- tempfile("test", fileext=".avro")
   spark_write_avro(df2, filename)
